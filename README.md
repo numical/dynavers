b391781fbcf92e9a4d42e6b984d875a7bc5996bd
Mike Evans <mike.evans@numical.com>
2016-09-26 14:25:15 +0100

    initial PoC, no tests

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# dynavers
-Dynamic Package Versions for Node
+# Dynamic Module Versions for Node
+
+Install multiple versions of a module then dynamically switch between them.
+```js
+const versions = ['1.13.1', '2.1.0-beta.24']
+for(const version of versions) {
+  setModuleVersion('webpack', version);
+  // ...do stuff
+}
+```
+As the above example hints at, this is primarily designed for testing.
+
+## Installation
+
+```bash
+npm install --save-dev dynavers
+```
+
+## Usage
+
+### Specifying which versions to install
+
+Create a JSON spec of modules to be installed, e.g. at `dynavers.json`:
+```json
+{
+  "path": "dynavers_modules",
+  "versions": {
+    "webpack": ["1.13.1", "2.1.0-beta.24"]
+  }
+}
+```
+Do not use fuzzy versions (`"^0.16.0"`) - this will cause problems.
+
+### Installing the modules from npm
+Next, run
+```bash
+./node_modules/.bin/install-module-versions dynavers.json
+```
+
+In this example, it will create `dynavers_modules` and install webpack
+1.13.1 and 2.1.0-beta.24 inside the `dynavers_modules` directory.
+
+To run the install automatically before your test suite (when you run `npm
+test`), add it as a "pretest" hook to your `package.json`:
+
+```json
+{
+  "scripts": {
+    "pretest": "install-module-versions dynavers.json",
+    "test": "..."
+  }
+}
+```
+
+`install-module-versions` will not redownload existing modules. If something went wrong,
+delete its directory first: `rm -r dynavers_modules`
+
+### `setModuleVersion`: Specifying module version
+The API is a single function `setModuleVersion(moduleName, moduleVersion)`.
+This will ensure any subsequent `require` calls to that module *from anywhere* will use the specified version.
+Note this will also ensure any exports within that module will also use the specified version:
+```js
+setModuleVersion('webpack', '1.13.1')
+var webpack = require('webpack')          // version 1.13.1
+var Chunk = require('weback/lib/Chunk')   // will also be the 1.13.1 version
+```
+
+## Caveats
+1. This works by monkey-patching `node`'s `Module` load mechanism.  You may not like this.
+2. Note the version number! Currently very much *alpha* code - no tests yet either!
+3. The `load` mechanism has not been tried with all exotic form of package name that `require` will
+   accept.  Please [let me know](https://github.com/numical/dynavers/issues) if any this fails.
+
+## Alternatives
+[multidep](https://github.com/joliss/node-multidep) offers an alternative that specifies the version
+for a single `require` call via its `multidepRequire`.
+
+## Acknowledgements
+This tool is based on [multidep](https://github.com/joliss/node-multidep) - thanks [Jo
+Liss](https://github.com/joliss)!  
+(I would have submitted a Pull Request but by the time I had changed the external API and the core mechanism, I don't think it would have been accepted).
---
+++ index.js
@@ -0,0 +1,118 @@
+'use strict'
+
+var fs = require('fs')
+var path = require('path')
+var spawn = require('spawn-cmd').spawn
+var RSVP = require('rsvp')
+var rimraf = require('rimraf')
+var Module = require('module')
+
+module.exports = function dynavers (specPath) {
+  var spec = getSpec(specPath)
+  var modules = {}
+  var specifiedModules
+
+  Object.keys(spec.versions).sort().forEach(function (moduleName) {
+    modules[moduleName] = {}
+    spec.versions[moduleName].forEach(function (version) {
+      var modulePath = path.join(spec.path, moduleName + '-' + version)
+      if (!fs.existsSync(modulePath)) {
+        throw new Error(modulePath + ': No such file or directory. Run `load-module-versions` to install.')
+      }
+      var absPath = fs.realpathSync(path.join(modulePath, 'node_modules', moduleName))
+      modules[moduleName][version] = absPath
+    })
+  })
+
+  function specifyModuleVersion (moduleName, version) {
+    if (modules[moduleName] == null) {
+      throw new Error("Module '" + moduleName + "' not found in " + specPath)
+    }
+    var versions = modules[moduleName]
+    if (versions[version] == null) {
+      throw new Error('Version ' + version + " of module '" + moduleName + "' not found in " + specPath)
+    }
+    if (!specifiedModules) {
+      specifiedModules = {}
+      startInterceptingLoad()
+    }
+    specifiedModules[moduleName] = versions[version]
+  }
+
+  function startInterceptingLoad () {
+    var original = Module._load
+    Module._load = function (request, parent, isMain) {
+      if (specifiedModules[request]) {
+        request = specifiedModules[request]
+      } else {
+        var index = request.indexOf(path.sep)
+        var first = request.slice(0, index)
+        if (index > -1 && specifiedModules[first]) {
+          request = path.join(specifiedModules[first], request.slice(index))
+        }
+      }
+      return original.call(this, request, parent, isMain)
+    }
+  }
+
+  return specifyModuleVersion
+}
+
+module.exports.install = function (specPath) {
+  var spec = getSpec(specPath)
+
+  if (!fs.existsSync(spec.path)) {
+    fs.mkdirSync(spec.path)
+  }
+
+  var promise = RSVP.resolve()
+  Object.keys(spec.versions).sort().forEach(function (moduleName) {
+    spec.versions[moduleName].forEach(function (version) {
+      promise = promise.then(function () {
+        var modulePath = path.join(spec.path, moduleName + '-' + version)
+        return RSVP.resolve()
+          .then(function () {
+            if (!fs.existsSync(modulePath)) {
+              console.log(moduleName + ' ' + version + ': Installing')
+              fs.mkdirSync(modulePath)
+              fs.mkdirSync(path.join(modulePath, 'node_modules'))
+              var cp = spawn('npm', ['install', moduleName + '@' + version], {
+                cwd: modulePath,
+                stdio: 'inherit',
+                timeout: 300
+              })
+              return new RSVP.Promise(function (resolve, reject) {
+                cp.on('exit', function (code, signal) {
+                  if (code !== 0 || signal != null) {
+                    reject(new Error('npm exited with exit code ' + code + ', signal ' + signal))
+                  } else {
+                    resolve()
+                  }
+                })
+              })
+            } else {
+              console.log(moduleName + ' ' + version + ': Installed')
+            }
+          })
+          .catch(function (err) {
+            // We created a nested promise with `RSVP.resolve()` above so this
+            // .catch clause only applies to the previous .then and doesn't
+            // catch earlier failures in the chain
+            rimraf.sync(modulePath)
+            throw err
+          })
+      })
+    })
+  })
+  return promise
+}
+
+function getSpec (specPath) {
+  // specPath is relative to cwd, so we need to call realpathSync
+  var spec = require(fs.realpathSync(specPath))
+  if (!spec || !spec.hasOwnProperty('path') || Array.isArray(spec.versions)) {
+    throw new Error('Invalid version spec; expected { path: "dynavers_modules", versions: { ... } }, got ' +
+      require('util').inspect(spec))
+  }
+  return spec
+}
